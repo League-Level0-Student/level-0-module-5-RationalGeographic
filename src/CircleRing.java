73d4fd5505d5b07a8f0fb222427c4253618d7965
RationalGeographic <league@WTS14.attlocal.net>
2017-12-28 14:13:12 -0800

    makeup
    qefjhcv

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -9,16 +9,24 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
-
+Robot max = new Robot();
        //  2. Set your robot’s position to x=150 and y=200
-
+max.moveTo(150, 200);
         // 3. Put the robot's pen down
-
+max.penDown();
         // 4. Set the robot’s speed to 10
-
+max.setSpeed(10);
         // 5. Do everything below here 360 times (use i as the counter)
-
+for (int i = 0; i < 360; i++) {
+	max.move(3);
+	max.turn(1);
+	if(i%20==0) {
+		for (int j = 0; j < 360; j++) {
+			max.move(1);
+			max.turn(1);
+		}
+	}
+}
 	        // 6. Move the robot 3 pixels
 
        //  7. Turn the robot 1 degree
