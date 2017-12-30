59aec7207cdb996646b48ff1fa61456d7b789243
RationalGeographic <league@WTS10.attlocal.net>
2017-12-30 14:25:34 -0800

    justmeh
    dhfghgthforloopgauntletrgbahfgbhfbg

---
+++ src/ForLoopGauntlent.java
@@ -0,0 +1,68 @@
+
+public class ForLoopGauntlent {
+	
+public static void main(String[] args) {
+/*
+ * #1
+for(int i = 0; i < 101; i++) {
+	System.out.println(i);
+}
+#2
+	for (int i = 100; i > 0; i--) {
+		System.out.println(i);
+	}
+	#3
+	for (int i = 2; i < 101; i++) {
+		if(i%2==0) {
+			System.out.println(i);
+		}
+		
+	}
+	#4
+	for (int i = 1; i < 100; i++) {
+		if(i%2==1) {
+			System.out.println(i);
+		}
+	}
+	#5
+	for (int i = 0; i < 501; i++) {
+		if(i%2==0) {
+			System.out.println(i + " is even");
+		}
+		else{
+			System.out.println(i + " is odd");
+		}
+	}
+	#6
+	for (int i = 1; i < 778; i++) {
+		if(i%7==0) {
+			System.out.println(i);
+		}
+	
+	}
+	#7
+	for (int i = 1; i < 14; i++) {
+		System.out.println("in " +(i + 2004) + " I was " + i + " years old" );
+	}
+	
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 3; j++) {
+			System.out.println(i + " " +j);
+		}
+		
+		
+	}
+	*/
+	for (int i = 0; i < 3; i++) {
+		
+		for (int j = 1; j < 4; j++) {
+			System.out.print(j + (i*3));
+			if(j==3) {
+				System.out.print("\n");
+			}
+		}
+		
+	}
+}
+
+}
